90a8beef058b5a6a0fab77fe92e4fa666249042f
clcannon <71797046+clcannon@users.noreply.github.com>
2022-04-13 20:19:27 -0700

    Maybe maybe maybe works
    Maybe creates edges correctly with attributes "topic" and "diff" (difference between posting dates, ie if diff is 3 days, 4 hours, the edge was created this long after the adjacent post)

--- create-network.py
+++ create-network.py
@@ -49,28 +49,56 @@ users_ids = []
 
 # find a way to remove this. its just getting a list of user id's. All one-liners tested returned lists of lists
 # or incorrectly shaped lists.
-for index, row in users.iterrows():
+for index, post in users.iterrows():
     # check if user_id is in relevant users, else continue
-    users_ids.append(row['users_id'])
+    users_ids.append(post['users_id'])
 
 threads = get_q(get_threads_query, ['topics_id', 'posts_id', 'users_id', 'posted_date'], 't_posts')
+
 
 def print_timing(section: str):
     global start
     print(f"{section:30}", ((time_ns() - start) // 1000000) / 1000, "s")
     start = time_ns()
 
+
 print_timing("Get from DB")
 
-g = nx.Graph()
-users_by_thread = {}
-count = 0
-# For each valid topic, get relevant data
-for index, row in threads.iterrows():
+g = nx.DiGraph()
+
+# dictionary for holding info as: key = topics_id, vals = users_id
+thread_info = {}
+
+# For each valid post, make relevant connections
+for index, post in threads.iterrows():
+    users_id = post['users_id']
+    topics_id = post['topics_id']
+    posted_date = post['posted_date']
+
     # ignore nodes that are not in the initially queried users list
-    if row['users_id'] not in users_ids:
+    if users_id not in users_ids:
         continue
-    count = count + 1
+
+    # add user node if not already in the graph
+    if not g.has_node(users_id):
+        g.add_node(users_id)
+
+    # add thread to thread_info dict
+    if topics_id not in thread_info:
+        thread_info[topics_id] = list()
+        thread_info[topics_id].append([users_id, posted_date])
+
+    # make edges between all those who posted in thread previous
+
+    for user, date in thread_info[topics_id]:
+        # prevents edges to self
+        if user == users_id:
+            continue
+        # edges save the difference in time between nodes with regards to a post
+        g.add_edge(users_id, user, topic=topics_id, diff=(posted_date - date))
+        print('' + users_id + ' ' + user + ' ' + g.get_edge_data(users_id, user))
 
 
-print(count)
+
+print(g.edges)
+print("ldkfhjsa")
